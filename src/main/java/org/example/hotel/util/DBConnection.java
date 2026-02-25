58490c169cf82cf9adc43601b44d249bcecbb790
pramodya17 <pramodayaudan17@gmail.com>
2026-02-25 10:48:32 +0530

    DBConnection.java and PasswordUtil.java

--- src/main/java/org/example/hotel/util/DBConnection.java
+++ src/main/java/org/example/hotel/util/DBConnection.java
@@ -1,4 +1,26 @@
 package org.example.hotel.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private static HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/hotel_db");
+        config.setUsername("root");
+        config.setPassword("password");
+        config.setMaximumPoolSize(10);
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 }
